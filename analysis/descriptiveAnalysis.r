9b072a37eace2749a7ab7afcbb4597f4bb473b67
Leixb <abone9999@gmail.com>
2020-10-25 12:00:34 +0100

    descriptiveAnalysis

--- analysis/descriptiveAnalysis.r
+++ analysis/descriptiveAnalysis.r
@@ -6,41 +6,59 @@ library(extrafont)
 library(showtext)
 library(cowplot)
 
-
-
-#loadfonts()
-font_add("LM Roman", regular = "latinmodern-math.otf")
-
-showtext_auto()
-
 dd <- readRDS('data/10-data_pre.Rda')
-
 n <- dim(dd)[1]
 
+theme_font <- theme()
+
+font_init <- function() {
+  loadfonts(quiet = T)
+  font_add("LM Roman", regular = "latinmodern-math.otf")
+
+  theme_font <<- theme(text = element_text(family = "LM Roman"))
+
+  showtext_auto()
+}
+
 ## Summary of numerical variables
+num_summary <- function() {
 
-tab <- t(summary(Filter(is.numeric, dd)))
-tab <- apply(tab, 2, function(x)
-  sub(".*:", "", x))
-colnames(tab) <-
-  c("Min", "1st Q.", "Median", "Mean", "3rd Q.", "Max", "NA's")
+  tab <- t(summary(Filter(is.numeric, dd)))
+  tab <- apply(tab, 2, function(x)
+    sub(".*:", "", x))
+  colnames(tab) <-
+    c("Min", "1st Q.", "Median", "Mean", "3rd Q.", "Max", "NA's")
 
-cat(kbl(tab, booktabs = T, format = 'latex') %>% #landscape()
-      kable_styling(latex_options = c("scale_down")),
-    file = 'tables/num_summary.tex')
+  cat(kbl(tab, booktabs = T, format = 'latex') %>% #landscape()
+        kable_styling(latex_options = c("scale_down")),
+      file = 'tables/num_summary.tex')
+
+}
 
 # Basic descriptive analysis for numerical variables
+plot_num <- function(k, bins=100, df=dd) {
+  bins <- min(bins, length(unique(df[, k])))
 
-theme_font <- theme(text = element_text(family = "LM Roman"))
-
-for (k in names(dd)) {
   var_s <- sym(k)
-  if (!(is.numeric(dd[, k]) || class(dd[, k]) == "Date")) {
-    frecs <- table(as.factor(dd[, k]), useNA = "ifany")
+
+  hi <- ggplot(df, aes(x = !!var_s)) + geom_histogram(bins = bins) + theme_font
+
+  bp <-
+    ggplot(df, aes(x = "", y = !!var_s)) + geom_boxplot() + coord_flip() +
+      theme_font + labs(x = NULL, y = NULL, fill = NULL)
+
+  plot_grid(bp, hi, align = "v", nrow = 2, rel_heights = c(1/4, 3/4))
+}
+
+# Basic descriptive analysis for factorial variables
+plot_fact <- function(k, frecs, df=dd) {
+
     proportions <- frecs / n
 
     dfc <- as.data.frame(proportions)
     colnames(dfc)[1] <- k
+
+    var_s <- sym(k)
 
     p <- ggplot(dfc, aes("", Freq, fill = !!var_s)) +
       geom_bar(
@@ -55,59 +73,44 @@ for (k in names(dd)) {
                 family = "LM Roman") +
       labs(x = NULL, y = NULL, fill = NULL) +
       guides(fill = guide_legend(reverse = TRUE)) +
-      #scale_fill_manual(values = c("#ffd700", "#bcbcbc", "#ffa500", "#254290")) +
+
       theme_classic() + theme_font +
       theme(
         axis.line = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank()
       )
+
     ggsave(plot = p,
            sprintf('plots/%s-pie.pdf', k),
            height = 2.5, width = 3.5)
 
-    p <-
-      ggplot(dd, aes(x = !!var_s, fill = !!var_s)) + geom_bar(width = 0.7) +
+    b <-
+      ggplot(df, aes(x = !!var_s, fill = !!var_s)) + geom_bar(width = 0.7) +
       theme(legend.position = "none",
             axis.text.x = element_text(angle = -90, hjust = 0))  + theme_font
-    ggsave(plot = p,
+
+    ggsave(plot = b,
            sprintf('plots/%s-bar.pdf', k),
            height = 2.5, width = 3.5)
 
-    table <-
-      kbl(sort(frecs, decreasing = T),
-          booktabs = T,
-          format = 'latex') %>%
-      kable_styling(position = "center")
-    cat(table, file = sprintf('tables/%s-freq.tex', k))
+}
 
-  } else {
-    if (class(dd[, k]) == "Date") {
-      print(summary(dd[, k]))
-      print(sd(dd[, k]))
+plot_save <- function(p, name, tail, width=5.5, height = 4) {
+  ggsave(plot = p,
+         file = sprintf('plots/%s-%s.pdf', name, tail),
+         device = 'pdf',
+         width = width,
+         height = height)
+}
 
-      p <-
-        ggplot(dd, aes(x = !!var_s)) + geom_histogram()  + theme_font
-      ggsave(plot = p,
-             sprintf('plots/%s-hist.pdf', k),
-             height = 4, width = 5.5)
+plot_all <- function() {
+  for (k in names(dd)) {
+    if (is.numeric(dd[, k])) {
 
-    } else {
-      hi <- ggplot(dd, aes(x = !!var_s)) + geom_histogram(bins = min(100, length(unique(dd[, k]))))  + theme_font
+      p <- plot_num(k)
 
-      bp <-
-        ggplot(dd, aes(x = "", y = !!var_s)) + geom_boxplot() + coord_flip() +
-          theme_font + labs(x = NULL, y = NULL, fill = NULL)
-
-
-      #pdf(sprintf('plots/%s-hi_bp.pdf', k))
-
-      g <- plot_grid(bp, hi, align = "v", nrow = 2, rel_heights = c(1/4, 3/4))
-
-      ggsave(plot = g, file = sprintf('plots/%s-hi_bp.pdf', k), device = 'pdf', width = 5.5, height = 4)
-      #grid.newpage()
-      #rid.draw(rbind(ggplotGrob(bp), ggplotGrob(hi), size = "last"))
-      #dev.off()
+      plot_save(p, k, 'hi_bp')
 
       tab <- data.frame(rbind(t(summary(dd[, k]))))
 
@@ -124,16 +127,31 @@ for (k in names(dd)) {
       cat(kbl(tab, booktabs = T, format = 'latex') %>% #landscape()
             kable_styling(latex_options = c("scale_down")),
           file = sprintf('tables/%s-ext_sum.tex', k))
+
+    } else {
+      frecs <- table(as.factor(df[, k]), useNA = "ifany")
+
+      plot_fact(k, frecs)
+
+      table <-
+        kbl(sort(frecs, decreasing = T),
+            booktabs = T,
+            format = 'latex') %>%
+        kable_styling(position = "center")
+      cat(table, file = sprintf('tables/%s-freq.tex', k))
+
     }
   }
 }
 
-df <- dd[dd$price < 500, ]
+# Additional plots with tweaks
+extra_plots <- function() {
+  p <- plot_num('price', df = dd[dd$price < 500, ])
 
-hi <- ggplot(df, aes(x = price)) + geom_histogram(bins=100)  + theme_font
+  plot_save(p, 'price', 'hi_bp-tallat500')
+}
 
-#pdf(sprintf('plots/%s-hi_bp.pdf', k))
-
-g <- plot_grid(bp, hi, align = "v", nrow = 2, rel_heights = c(1/4, 3/4))
-
-ggsave(plot = g, file = sprintf('plots/%s-hi_bp-tallat500.pdf', 'price'), device = 'pdf', width = 5.5, height = 4)
+num_summary()
+font_init()
+plot_all()
+extra_plots()
